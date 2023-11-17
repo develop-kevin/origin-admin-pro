725ec699c5f2262ae7ace3eda13d861803830638
1129759063@qq.com <1129759063@qq.com>
2023-11-17 15:15:41 +0800

    删除文件目录

--- src/main/java/com/origin/admin/modules/system/controller/validate/ValidatedAction.java
+++
@@ -1,8 +0,0 @@
-package com.origin.admin.modules.system.controller.validate;/**
- * @Author: Kevin
- * @Email: 1129759063@qq.com
- * @Description: 
- * @Date 2023/11/16 19:00
- */
-public class ValidatedAction {
-}
